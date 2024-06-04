8b992d16ae76fa085e215b1d7627b4db8a7b7149
7859897243 <sosadhruv740@gmail.com>
2024-06-04 18:50:55 +0530

    add bootstrap

---
+++ module 2.mjs
@@ -0,0 +1,9 @@
+const a = "dhruv"
+const b = "dhaval"
+const c = "prince"
+const d = "parth"
+
+export default a;
+export {b};
+export {c};
+export {d};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 
 import './App.css';
-import img1 from './images/img1.jpg';  
+
 
 function App() {
   return (
     <>
-    <h1>SIT</h1>
-    <header>
-      <nav>
-        <ul>
-          <li>home</li>
-          <li>about</li>
-          <li>contact us </li>
-        </ul>
-      </nav>
-      <p>dsfkbsdkbkfkfBWRKBVKBJBRTIE dsfkbsdkbkfkfBWRKBVKBJBRTIEds  fkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIE</p>
-      <img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"></img>
-      <img src={img1}></img>
-    </header>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">DHRUv</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+       
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </>
   );
 }
