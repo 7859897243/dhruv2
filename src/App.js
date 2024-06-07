2e4ce9598857f32f7aa7f5371a61bb7c3510156d
7859897243 <sosadhruv740@gmail.com>
2024-06-07 17:25:04 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-
-
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-    
+    <Navbar title="SIT" aboutText="Contact"/>
+    <TextForm heading="enter your text here"/>
     </>
   );
 }
