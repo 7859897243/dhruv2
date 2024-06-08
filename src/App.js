3b6bf3f4137248d0164d8966cebb920f1195a00b
7859897243 <sosadhruv740@gmail.com>
2024-06-08 17:01:46 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
+import AboutUS from './components/AboutUS';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-
+// import TextForm from './components/TextForm';
 function App() {
   return (
     <>
     <Navbar title="SIT" aboutText="Contact"/>
-    <TextForm heading="enter your text here"/>
+    {/* <TextForm heading="enter your text here"/> */}
+    <AboutUS heading="About Us" />
     </>
   );
 }
