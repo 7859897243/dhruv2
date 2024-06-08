3a56ccf4fe670af6024e20ad97b5d2d833bd4418
7859897243 <sosadhruv740@gmail.com>
2024-06-08 17:38:00 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm.js';
 
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.background= 'black'
+    } else {
+      setMode('light');
+      document.body.style.background= 'white'
+    }
+  }
   return (
     <>
-    <Navbar title="SIT" aboutText="Contact"/>
+    <Navbar title="SIT" aboutText="Contact us" mode={mode} toggleMode = {toggleMode}/>
     <TextForm heading="enter your text here"/>
     </>
   );
