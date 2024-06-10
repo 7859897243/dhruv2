372b69d02aa9db442da17405011f95695da3c127
7859897243 <sosadhruv740@gmail.com>
2024-06-10 17:03:43 +0530

    add

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ function App() {
 
   const [mode, setMode] = useState('light')
 
+  const [btnText, newbtnText]= useState('Enable dark mode ')
+
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.background= 'black'
+      newbtnText('Enable dark mode')
     } else {
       setMode('light');
       document.body.style.background= 'white'
+      newbtnText('Enable dark mode')
     }
   }
   return (
     <>
-    <Navbar title="SIT" aboutText="Contact us" mode={mode} toggleMode = {toggleMode}/>
-    <TextForm heading="enter your text here"/>
+    <Navbar title="SIT" aboutText="Contact us" mode={mode} toggleMode = {toggleMode} btnText={btnText}/>
+    <TextForm heading="enter your text here" mode={mode} toggleMode={toggleMode}/>
     </>
   );
 }
