382725df0b135b718ce26301976b11775530017b
7859897243 <sosadhruv740@gmail.com>
2024-06-12 15:54:29 +0530

    add

--- src/App.js
+++ src/App.js
@@ -6,8 +6,23 @@ import TextArea from './components/TextArea';
 import img1 from './images/img1.jpg';  
 import AboutUss from './components/AboutUss';
 
-function App() {
+  function App() {
 
+    const [mode, setMode] = useState('light')
+  
+    const [btnText, newbtnText]= useState('Enable dark mode ')
+  
+    const toggleMode = () => {
+      if (mode === 'light') {
+        setMode('dark');
+        document.body.style.background= 'black'
+        newbtnText('Enable dark mode')
+      } else {
+        setMode('light');
+        document.body.style.background= 'white'
+        newbtnText('Enable dark mode') 
+      }
+    }
   
   return (
   
