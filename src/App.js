c2018eb79ac83aedb12cd9184b230bfa93b1d681
7859897243 <sosadhruv740@gmail.com>
2024-06-12 16:39:11 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 import img1 from './images/img1.jpg';  
 
 function App() {
   return (
     <>
-    <h1>SIT</h1>
-    <header>
-      <nav>
-        <ul>
-          <li>home</li>
-          <li>about</li>
-          <li>contact us </li>
-        </ul>
-      </nav>
-      <p>dsfkbsdkbkfkfBWRKBVKBJBRTIE dsfkbsdkbkfkfBWRKBVKBJBRTIEds  fkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIEdsfkbsdkbkfkfBWRKBVKBJBRTIE</p>
-      <img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"></img>
-      <img src={img1}></img>
-    </header>
+    <Navbar />    
     </>
   );
 }
